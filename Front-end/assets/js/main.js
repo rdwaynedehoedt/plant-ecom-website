d2b120103d3294a725fb0e2751c4e4fe2be70c30
Dwayne Dehoedt <dwaynedehoedt.rosch@gmail.com>
2024-10-19 23:45:00 +0530

    updated the product page

--- Front-end/assets/js/main.js
+++ Front-end/assets/js/main.js
@@ -131,12 +131,15 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 2500,
     delay: 400,
-    // reset: true
+    reset: true
 })
+
 
 sr.reveal(`.home__data`)
 sr.reveal(`.home__img`, { delay: 500 })
 sr.reveal(`.home__social`, { delay: 600 })
+sr.reveal(`.section__title-center`)
+sr.reveal(`.product__description`)
 sr.reveal(`.about__img, .contact__box`, { origin: 'left' })
 sr.reveal(`.about__data, .contact__form`, { origin: 'right' })
 sr.reveal(`.steps__card, .product__card, .questions__group, .footer`, { interval: 100 })
