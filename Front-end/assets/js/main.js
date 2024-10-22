e919723c6b7ea9f5ad1ea3745ba69bfdef3c79f0
Dwayne Dehoedt <dwaynedehoedt.rosch@gmail.com>
2024-10-22 20:21:20 +0530

    updates on login page

--- Front-end/assets/js/main.js
+++ Front-end/assets/js/main.js
@@ -144,3 +144,5 @@ sr.reveal(`.product__description`)
 sr.reveal(`.about__img, .contact__box`, { origin: 'left' })
 sr.reveal(`.about__data, .contact__form`, { origin: 'right' })
 sr.reveal(`.steps__card, .product__card, .questions__group, .footer`, { interval: 100 })
+
+sr.reveal(`.section__title-center`)
