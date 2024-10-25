2271da316e588ef513a67bc1668d212f905d2896
Dwayne Dehoedt <dwaynedehoedt.rosch@gmail.com>
2024-10-25 09:30:12 +0530

    udates

--- Back-end/auth/login.php
+++ Back-end/auth/login.php
@@ -5,6 +5,15 @@
     include('../config/db.php')
 
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-        
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+    }//here we will get the user name and the pw in to and arr like extract and stroring wage 
+
+    if(!empty($email) && !empty($password)){
     }
+    else
+    {
+        echo"Please Fill in both email and password fields!"
+    }
+    
 ?>
