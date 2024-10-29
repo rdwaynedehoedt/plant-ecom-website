42552a0157273a02157253fadff3ed7727bbb8ab
Dwayne Dehoedt <dwaynedehoedt.rosch@gmail.com>
2024-10-29 16:32:50 +0530

    back end user log in complited

--- Back-end/dashboard/dashboard.php
+++ Back-end/dashboard/dashboard.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 if (!isset($_SESSION['user_id'])) {
-    // If not logged in, redirect to login page
-    header("Location: login.html");
+    // If the user is not logged in, redirect to login page
+    header("Location: /Front-end/login.html");
     exit();
 }
 ?>
@@ -16,5 +16,6 @@ if (!isset($_SESSION['user_id'])) {
 <body>
     <h2>Welcome, <?php echo $_SESSION['email']; ?>!</h2>
     <p>You are logged in.</p>
+    <a href="/Back-end/auth/logout.php">Logout</a>
 </body>
 </html>
