36348a9d037643f51899e0da32b30e7058080181
Dwayne Dehoedt <dwaynedehoedt.rosch@gmail.com>
2024-12-05 11:17:31 +0530

    updates

--- Back-end/config/db.php
+++ Back-end/config/db.php
@@ -1,14 +1,12 @@
 <?php
-// Database connection parameters
-$host = 'localhost';       // Typically 'localhost' for local development
-$db_user = 'root';         // MySQL username (default is 'root' for XAMPP)
-$db_pass = '';             // MySQL password (default is empty for XAMPP)
+
+$host = 'localhost';       
+$db_user = 'root';        
+$db_pass = '';            
 $db_name = 'plant_ecom_db';
 
-// Create a connection
 $mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
 
-// Check the connection
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
