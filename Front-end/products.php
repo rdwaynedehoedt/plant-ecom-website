03cef144bae81a401272f9c7f0fbbd496522280c
Dwayne <dwaynedehoedt.rosch@gmail.com>
2024-12-05 22:52:24 +0530

    updated on prducts with db

--- Front-end/products.php
+++ Front-end/products.php
@@ -1,30 +1,12 @@
 <?php
 session_start();
 
-include ('/xampp/htdocs/plant-ecom-website/Back-end/config/db.php');
+include('/xampp/htdocs/plant-ecom-website/Back-end/config/db.php');
 
-$stm = $mysqli->prepare("SELECT * FROM products WHERE id =  ?");
-$stm->bind_param("i", $_GET['id']);
+$stm = $mysqli->prepare("SELECT * FROM products");
 $stm->execute();
 $result = $stm->get_result();
-
-if ($result->num_rows >= 1) {
-    $product = $result->fetch_assoc();
-
-    $product_id = $product['id'];
-    $product_name = $product['name'];
-    $product_price = $product['price'];
-    $product_description = $product['description'];
-    $product_image = $product['image_url'];
-    $product_plant_type = $product['plant_type'];
-    $product_size = $product['size'];
-    $product_watering = $product['watering'];
-    $product_light = $product['light'];
-
-} else {
-    echo "No products found!";
-    exit();
-}
+$products = $result->fetch_all(MYSQLI_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -41,167 +23,85 @@ if ($result->num_rows >= 1) {
 
 <body>
 <header class="header" id="header">
-        <nav class="nav container">
-            <a href="/PLANT-ECOM-WEBSITE/Front-end/index.php" class="nav__logo">
-                <i class="ri-leaf-line nav__logo-icon"></i> PlantPedia
-            </a>
+    <nav class="nav container">
+        <a href="/PLANT-ECOM-WEBSITE/Front-end/index.php" class="nav__logo">
+            <i class="ri-leaf-line nav__logo-icon"></i> PlantPedia
+        </a>
 
-            <div class="nav__menu" id="nav-menu">
-                <ul class="nav__list">
-                    <li class="nav__item"><a href="/PLANT-ECOM-WEBSITE/Front-end/index.php" class="nav__link">Home</a></li>
-                    <li class="nav__item"><a href="/PLANT-ECOM-WEBSITE/Front-end/about.php" class="nav__link">About</a></li>
-                    <li class="nav__item"><a href="/PLANT-ECOM-WEBSITE/Front-end/products.php" class="nav__link">Products</a></li>
-                    <li class="nav__item"><a href="/PLANT-ECOM-WEBSITE/Front-end/contact.php" class="nav__link">Contact Us</a></li>
-                </ul>
+        <div class="nav__menu" id="nav-menu">
+            <ul class="nav__list">
+                <li class="nav__item"><a href="/PLANT-ECOM-WEBSITE/Front-end/index.php" class="nav__link">Home</a></li>
+                <li class="nav__item"><a href="/PLANT-ECOM-WEBSITE/Front-end/about.php" class="nav__link">About</a></li>
+                <li class="nav__item"><a href="/PLANT-ECOM-WEBSITE/Front-end/products.php" class="nav__link">Products</a></li>
+                <li class="nav__item"><a href="/PLANT-ECOM-WEBSITE/Front-end/contact.php" class="nav__link">Contact Us</a></li>
+            </ul>
 
-                <div class="nav__close" id="nav-close">
-                    <i class="ri-close-line"></i>
-                </div>
+            <div class="nav__close" id="nav-close">
+                <i class="ri-close-line"></i>
             </div>
+        </div>
 
-            <div class="nav__btns">
-                   <i class="ri-moon-line change-theme" id="theme-button"></i>
-
-                <div class="nav__toggle" id="nav-toggle">
-                    <i class="ri-menu-line"></i>
-                </div>
+        <div class="nav__btns">
+            <i class="ri-moon-line change-theme" id="theme-button"></i>
+            <div class="nav__toggle" id="nav-toggle">
+                <i class="ri-menu-line"></i>
             </div>
-            <div class="nav__btns">
             <?php if (isset($_SESSION['user_id'])): ?>
+                <a href="/PLANT-ECOM-WEBSITE/Back-end/auth/logout.php" class="Login_button--flex">
+                    Logout <i class="ri-logout-box-r-line button__icon"></i>
+                </a>
+                <?php if ($_SESSION['role'] == 'admin'): ?>
+                    <a href="/PLANT-ECOM-WEBSITE/Back-end/dashboard/admin/admin-dashboard.php" class="Login_button--flex">
+                        Dashboard <i class="ri-dashboard-line button__icon"></i>
+                    </a>
+                <?php elseif ($_SESSION['role'] == 'customer'): ?>
+                    <a href="/PLANT-ECOM-WEBSITE/Back-end/dashboard/customer/customer-dashboard.php" class="Login_button--flex">
+                        Profile <i class="ri-user-3-line button__icon"></i>
+                    </a>
+                <?php endif; ?>
+            <?php else: ?>
+                <a href="login.php" class="Login_button--flex">
+                    Login <i class="ri-login-box-line button__icon"></i>
+                </a>
+            <?php endif; ?>
+        </div>
+    </nav>
+</header>
 
-    <a href="/PLANT-ECOM-WEBSITE/Back-end/auth/logout.php" class="Login_button--flex">
-        Logout <i class="ri-logout-box-r-line button__icon"></i>
-    </a>
-    <?php if ($_SESSION['role'] == 'admin'): ?>
-        <a href="/PLANT-ECOM-WEBSITE/Back-end/dashboard/admin/admin-dashboard.php" class="Login_button--flex">
-            Dashboard <i class="ri-dashboard-line button__icon"></i>
-        </a>
-    <?php elseif ($_SESSION['role'] == 'customer'): ?>
-        <a href="/PLANT-ECOM-WEBSITE/Back-end/dashboard/customer/customer-dashboard.php" class="Login_button--flex">
-            Profile <i class="ri-user-3-line button__icon"></i>
-        </a>
-    <?php endif; ?>
-<?php else: ?>
-    <a href="login.php" class="Login_button--flex">
-        Login <i class="ri-login-box-line button__icon"></i>
-    </a>
-<?php endif; ?>
+<section class="product section container" id="products">
+    <h2 class="section__title-center">
+        Check out our <br> products
+    </h2>
 
+    <p class="product__description">
+        Here are some selected plants from our showroom, all are in excellent shape and have a long life span. Buy and enjoy the best quality.
+    </p>
 
-</div>
-
-        </nav>
-    </header>
-
-    <section class="product section container" id="products">
-        <h2 class="section__title-center">
-            Check out our <br> products
-        </h2>
-
-        <p class="product__description">
-            Here are some selected plants from our showroom, all are in excellent
-            shape and has a long life span. Buy and enjoy best quality.
-        </p>
-
-        <div class="product__container grid">
+    <div class="product__container grid">
+        <?php foreach ($products as $product): ?>
             <article class="product__card">
+                <a href="product.php?id=<?php echo $product['id']; ?>">
                 <div class="product__circle"></div>
 
-                <img src="assets/img/product1.png" alt="" class="product__img">
+                <img src="<?php echo htmlspecialchars($product['image_url']); ?>" alt="<?php echo htmlspecialchars($product['name']); ?>" class="product__img">
 
-                <h3 class="product__title">Cacti Plant</h3>
-                <span class="product__price">$19.99</span>
+                <h3 class="product__title"><?php echo htmlspecialchars($product['name']); ?></h3>
+                <span class="product__price">$<?php echo number_format($product['price'], 2); ?></span>
 
-                <a href="p1.html" class="button--flex product__button">
+                <a href="#" class="button--flex product__button">
                     <i class="ri-shopping-bag-line"></i>
                 </a>
+        </a>
             </article>
+        <?php endforeach; ?>
+    </div>
+</section>
 
-           
-        </div>
-    </section>
+<footer class="footer section">
+    <!-- Footer content here -->
+</footer>
 
-    <footer class="footer section">
-        <div class="footer__container container grid">
-            <div class="footer__content">
-                <a href="#" class="footer__logo">
-                    <i class="ri-leaf-line footer__logo-icon"></i> PlantPedia
-                </a>
-
-                <h3 class="footer__title">
-                    Subscribe to our newsletter <br> to stay update
-                </h3>
-
-                <div class="footer__subscribe">
-                    <input type="email" placeholder="Enter your email" class="footer__input">
-
-                    <button class="button button--flex footer__button">
-                        Subscribe
-                        <i class="ri-arrow-right-up-line button__icon"></i>
-                    </button>
-                </div>
-            </div>
-
-            <div class="footer__content">
-                <h3 class="footer__title">Our Address</h3>
-
-                <ul class="footer__data">
-                    <li class="footer__information"> No 23 Galle Road</li>
-                    <li class="footer__information">Colombo 02</li>
-                    <li class="footer__information">Sri Lanka</li>
-                </ul>
-            </div>
-
-            <div class="footer__content">
-                <h3 class="footer__title">Contact Us</h3>
-
-                <ul class="footer__data">
-                    <li class="footer__information">+94 772186241</li>
-
-                    <div class="footer__social">
-                        <a href="https://www.facebook.com/" class="footer__social-link">
-                            <i class="ri-facebook-fill"></i>
-                        </a>
-                        <a href="https://www.instagram.com/" class="footer__social-link">
-                            <i class="ri-instagram-line"></i>
-                        </a>
-                        <a href="https://twitter.com/" class="footer__social-link">
-                            <i class="ri-twitter-fill"></i>
-                        </a>
-                    </div>
-                </ul>
-            </div>
-
-            <div class="footer__content">
-                <h3 class="footer__title">
-                    We accept all credit cards
-                </h3>
-
-                <div class="footer__cards">
-                    <img src="assets/img/card1.png" alt="" class="footer__card">
-                    <img src="assets/img/card2.png" alt="" class="footer__card">
-                    <img src="assets/img/card3.png" alt="" class="footer__card">
-                    <img src="assets/img/card4.png" alt="" class="footer__card">
-                </div>
-            </div>
-        </div>
-
-        <p class="footer__copy">&#169; DwayneFX 2024. All rigths reserved</p>
-    </footer>
-
-
-    <!--=============== SCROLL UP ===============-->
-    <a href="#" class="scrollup" id="scroll-up">
-        <i class="ri-arrow-up-fill scrollup__icon"></i>
-    </a>
-
-    <!--=============== SCROLL REVEAL ===============-->
-    <script src="assets/js/scrollreveal.min.js"></script>
-
-    <!--=============== MAIN JS ===============-->
-    <script src="assets/js/main.js"></script>
+<script src="assets/js/scrollreveal.min.js"></script>
+<script src="assets/js/main.js"></script>
 </body>
-
-</body>
-
 </html>
